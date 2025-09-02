bc9631b863272e7cdf06bfbebfb6053b463b7f48
RemzieK <neprotivokonstitu@gmail.com>
2025-09-02 13:25:58 +0300

    Implemented classes Player, Position and Direction

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,33 @@
+﻿namespace ChessLogic
+{
+    public class Direction
+    {
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Left = new Direction(0, -1);
+        //diagonals
+        public readonly static Direction UpRigth = Up + Right;
+        public readonly static Direction UpLeft = Up + Left;
+        public readonly static Direction DownRigth = Down + Right;
+        public readonly static Direction DownLeft = Down + Left;
+
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction a, Direction b)
+        {
+            return new Direction(a.RowDelta + b.RowDelta, a.ColumnDelta + b.ColumnDelta);
+        }
+        public static Direction operator *(int scalar, Direction dir)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+        }
+    }
+}
--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cea724b4f1ff2bd46c2321d9909f4c4a1c7e2a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
