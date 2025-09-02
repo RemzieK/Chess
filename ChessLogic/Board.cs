d49b30b506e289aaaed36acd17b8a452035bce3b
RemzieK <neprotivokonstitu@gmail.com>
2025-09-02 14:15:06 +0300

    Added pieces types and initial board with start pieces

---
+++ ChessLogic/Board.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Board
+    {
+        // rectangular array for storing the pieces
+        private readonly Piece[,] pieces = new Piece[8, 8];
+
+        public Piece this[int row, int col]
+        {
+            get { return pieces[row, col]; }
+            set { pieces[row, col] = value; }
+        }
+
+        public Piece this[Position pos]
+        {
+            get { return this[pos.Row, pos.Col]; }
+            set { this[pos.Row, pos.Col] = value; }
+        }
+
+        //retun a board with all the pieces put correctly
+        public static Board Initial()
+        {
+            Board board = new Board();
+            board.AddStartPieces();
+            return board;
+        }
+
+        //The start pieces on their places by coordinates
+        private void AddStartPieces()
+        {
+            //black pieces on top row
+            this[0, 0] = new Rook(Player.Black);
+            this[0, 1] = new Knight(Player.Black);
+            this[0, 2] = new Bishop(Player.Black);
+            this[0, 3] = new Queen(Player.Black);
+            this[0, 4] = new King(Player.Black);
+            this[0, 5] = new Bishop(Player.Black);
+            this[0, 6] = new Knight(Player.Black);
+            this[0, 7] = new Rook(Player.Black);
+
+            //white pieces on bottom row
+            this[7, 0] = new Rook(Player.White);
+            this[7, 1] = new Knight(Player.White);
+            this[7, 2] = new Bishop(Player.White);
+            this[7, 3] = new Queen(Player.White);
+            this[7, 4] = new King(Player.White);
+            this[7, 5] = new Bishop(Player.White);
+            this[7, 6] = new Knight(Player.White);
+            this[7, 7] = new Rook(Player.White);
+
+            //adding the pawns for both players using a loop
+            for(int c = 0; c < 8; c++)
+            {
+                this[1, c] = new Pawn(Player.Black);
+                this[6, c] = new Pawn(Player.White); 
+            }
+
+        }
+
+        public static bool IsInside(Position pos)
+        {
+            return pos.Row >= 0 && pos.Row < 8 && pos.Col >= 0 && pos.Col < 8;
+        }
+
+        public bool IsEmpty(Position pos)
+        {
+            return this[pos] == null;
+        }
+    }
+}
