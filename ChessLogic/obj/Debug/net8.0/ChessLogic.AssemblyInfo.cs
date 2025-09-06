dfa2d1c9a2048632cc166afa8c17c14a9c8ce153
RemzieK <neprotivokonstitu@gmail.com>
2025-09-06 21:11:50 +0300

    UI Window resize

--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9631b863272e7cdf06bfbebfb6053b463b7f48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d49b30b506e289aaaed36acd17b8a452035bce3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
