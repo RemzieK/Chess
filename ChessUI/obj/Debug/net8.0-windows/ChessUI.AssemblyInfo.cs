9558880d907cc189136c310693d661cdef7bfe48
RemzieK <neprotivokonstitu@gmail.com>
2025-09-24 13:25:33 +0300

    Game over menu UI

--- ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c2649579185d783a527f77fb4d5a86238aeea6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6276bbb3a902c1402c9a3dd97cd54444c92a31df")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
